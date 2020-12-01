e4d805bc8f3966e8dbbb2c5c766459c7f55a6aff
dcbryant <cong.ding@100credit.com>
2020-12-01 14:29:55 +0800

    feat(commitlint): add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "@commitlint/config-conventional", // 符合 Angular团队规范
+    // "cz"
+  ],
+  rules: {
+    "type-enum": [2, "always", [
+      "feat",
+      "fix",
+      "config",
+      "refactor",
+      "pref",
+      "docs",
+      "chore",
+      "style",
+      "revert"
+    ]],
+  }
+};
