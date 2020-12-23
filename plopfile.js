b57e92ccf4e06b735dad739adf7301b6fc69c4fc
dcbryant <cong.ding@100credit.com>
2020-12-23 16:06:46 +0800

    feat(plop): 引入plop

---
+++ plopfile.js
@@ -0,0 +1,43 @@
+module.exports = function(plop) {
+	// controller generator
+	plop.addHelper("lowerCase", function(p) {
+		return p.toLowerCase().trim();
+	});
+	plop.setGenerator("component", {
+		description: "create component",
+		prompts: [
+			{
+				type: "input",
+				name: "name",
+				message: "component name please"
+			}
+		],
+		actions: [
+			{
+				type: "add",
+				path: "src/components/{{name}}/{{lowerCase name}}.tsx",
+				templateFile: "plop-template/component.hbs"
+			},
+			{
+				type: "add",
+				path: "src/components/{{name}}/index.tsx",
+				templateFile: "plop-template/componentIndex.hbs"
+			},
+			{
+				type: "add",
+				path: "src/components/{{name}}/_style.scss",
+				templateFile: "plop-template/componentStyle.hbs"
+			},
+			{
+				type: "add",
+				path: "src/components/{{name}}/{{lowerCase name}}.stories.tsx",
+				templateFile: "plop-template/componentStories.hbs"
+			},
+			{
+				type: "add",
+				path: "src/components/{{name}}/{{lowerCase name}}.md",
+				templateFile: "plop-template/componentMd.hbs"
+			}
+		]
+	});
+};
---
+++ src/components/Badge/badge.stories.tsx
@@ -0,0 +1,6 @@
+import  Badge  from './badge';
+
+export default {
+	title: 'Badge ',
+	component: { Badge },
+};
