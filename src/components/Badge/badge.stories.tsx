a86d26c489334fbfb282f1400011ecd0292cdc6e
dcbryant <cong.ding@100credit.com>
2020-12-24 15:14:25 +0800

    feat(badge): add badge

--- src/components/Badge/badge.stories.tsx
+++ src/components/Badge/badge.stories.tsx
@@ -1,6 +1,13 @@
+import React from "react";
 import  Badge  from './badge';
 
 export default {
 	title: 'Badge ',
 	component: { Badge },
 };
+
+export const Normal = () => (
+	<>
+		<Badge count={10} ></Badge>
+	</>
+);
--- src/components/Badge/badge.tsx
+++ src/components/Badge/badge.tsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { DOMAttributes, ReactNode, useRef, useEffect } from 'react'
+import classNames from "classnames";
 
-
-function Badge(){
-    return <div></div>
+export interface BadgeProps extends DOMAttributes<HTMLDivElement> {
+  type?:
+  | "primary"
+  | "default"
+  | "danger"
+  | "secondary"
+  | "success"
+  | "info"
+  | "light"
+  | "warning"
+  | "dark";
+  className?: string;
+  refCallback?: (e: HTMLDivElement | null) => void;
+  count?: ReactNode;
+  visible?: boolean;
+  dot?: boolean;
 }
+
+const baseClass = 'rc-badge';
+
+export function Badge(props: BadgeProps) {
+  const { type, className, refCallback, count, visible, dot, children, ...restProps } = props;
+  console.log(type, "type");
+  const classes = classNames('rc-badge', className, {
+    [`${baseClass}-${type}`]: type,
+  });
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (refCallback && ref.current) {
+      refCallback(ref.current)
+    }
+  }, [refCallback]);
+
+  return (
+    <div className={classes} ref={ref} {...restProps}>
+      {(count || dot) ? (
+        <div
+          className={`rc-badge-count rc-count-${type}
+            ${children ? "" : "no-children"} ${visible ? "" : "badge-hide"} ${dot ? "badge-dot" : ""
+            }`}
+        >
+          <span>{count}</span>
+        </div>
+      ) : null}
+      {children ? children : null}
+    </div>
+  )
+}
+
+Badge.defaultProps = {
+	type: "danger",
+	visible: true,
+	dot: false,
+	count: ""
+};
 
 export default Badge;
